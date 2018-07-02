4e23627156538bb40312c43aed58cbba73438fc0
Matthew Muccio <me@matthewmuccio.com>
2018-07-02 10:47:44 -0400

    Adds necessary functionality to the main controller. /add, /subtract, /multiply, and /divide URIs now route to the correct template or a page with the correct JSON response.

--- run/core/controllers/main.py
+++ run/core/controllers/main.py
@@ -6,18 +6,38 @@ from flask import Blueprint, jsonify, redirect, render_template, request, sessio
 
 controller = Blueprint("main", __name__, url_prefix="/")
 
+@controller.route("", methods=["GET"])
+def show_main():
+    return render_template("main.html")
+
 @controller.route("/add", methods=["GET"])
 def add():
-    return render_template("main.html")
+    num1 = request.args.get("num1", default=0.0, type=float)
+    num2 = request.args.get("num2", default=0.0, type=float)
+    expression = "{0} + {1}".format(num1, num2)
+    result = "{0}".format(num1 + num2)
+    return jsonify(expression=expression, result=result)
 
 @controller.route("/subtract", methods=["GET"])
 def subtract():
-    return render_template("main.html")
+    num1 = request.args.get("num1", default=0.0, type=float)
+    num2 = request.args.get("num2", default=0.0, type=float)
+    expression = "{0} - {1}".format(num1, num2)
+    result = "{0}".format(num1 - num2)
+    return jsonify(expression=expression, result=result)
 
 @controller.route("/multiply", methods=["GET"])
 def multiply():
-    return render_template("main.html")
+    num1 = request.args.get("num1", default=0.0, type=float)
+    num2 = request.args.get("num2", default=0.0, type=float)
+    expression = "{0} * {1}".format(num1, num2)
+    result = "{0}".format(num1 * num2)
+    return jsonify(expression=expression, result=result)
 
 @controller.route("/divide", methods=["GET"])
 def divide():
-    return render_template("main.html")
+    num1 = request.args.get("num1", default=0.0, type=float)
+    num2 = request.args.get("num2", default=0.0, type=float)
+    expression = "{0} / {1}".format(num1, num2)
+    result = "{0}".format(num1 / num2)
+    return jsonify(expression=expression, result=result)
